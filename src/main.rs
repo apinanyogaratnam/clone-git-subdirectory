f2c56d30abc258d6df1d14fe7a73063ce01bb7ef
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-23 20:24:25 -0500

    feat: printing command

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() {
 
     let mut command = "git clone ".to_owned();
     command += query;
-    // !NOTE: command = "git clone https://github.com/vercel/next.js/tree/deprecated-main/examples/active-class-name"
 
     let indexes_of_slash: Vec<_> = command[29..].match_indices("/").collect();
     let index_of_subdirectory = indexes_of_slash[1].0;
@@ -20,16 +19,18 @@ fn main() {
     let repository_url = &command[10..29 + index_of_subdirectory];
     println!("{:?}", repository_url);
 
+    command = command[..29+index_of_subdirectory].to_string();
+    println!("{:?}", command);
     let mut list_dir = Command::new("ls");
 
-    // Execute `ls` in the current directory of the program.
-    list_dir.status().expect("process failed to execute");
+    // // Execute `ls` in the current directory of the program.
+    // list_dir.status().expect("process failed to execute");
 
-    println!();
+    // println!();
 
-    // Change `ls` to execute in the root directory.
-    list_dir.current_dir("/");
+    // // Change `ls` to execute in the root directory.
+    // list_dir.current_dir("/");
 
-    // And then execute `ls` again but in the root directory.
-    list_dir.status().expect("process failed to execute");
+    // // And then execute `ls` again but in the root directory.
+    // list_dir.status().expect("process failed to execute");
 }
