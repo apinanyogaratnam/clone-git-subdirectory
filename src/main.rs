08c45ac2fefa426ad13e470831891bc8c3fd9490
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-23 19:23:14 -0500

    feat: listing files in current and root directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut list_dir = Command::new("ls");
+
+    // Execute `ls` in the current directory of the program.
+    list_dir.status().expect("process failed to execute");
+
+    println!();
+
+    // Change `ls` to execute in the root directory.
+    list_dir.current_dir("/");
+
+    // And then execute `ls` again but in the root directory.
+    list_dir.status().expect("process failed to execute");
 }
