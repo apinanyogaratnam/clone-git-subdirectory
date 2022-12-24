30e84b37fb9cd414564c5af7b4be4f3ac0770079
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-23 20:29:05 -0500

    feat: cloning base repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::process::Command;
 use std::env;
 
+fn execute_command(command: &str) {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("failed to execute process");
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -17,16 +26,10 @@ fn main() {
     let index_of_subdirectory = indexes_of_slash[1].0;
 
     let repository_url = &command[10..29 + index_of_subdirectory];
-    println!("{:?}", repository_url);
 
     command = command[..29+index_of_subdirectory].to_string();
-    println!("{:?}", command);
-    let mut list_dir = Command::new("ls");
 
-    // // Execute `ls` in the current directory of the program.
-    // list_dir.status().expect("process failed to execute");
-
-    // println!();
+    execute_command(&command);
 
     // // Change `ls` to execute in the root directory.
     // list_dir.current_dir("/");
