2ab3e33019a0397d02d382efa38b361367325c59
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-23 21:28:25 -0500

    feat: created cli tool

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,53 @@ fn main() {
     let indexes_of_slash: Vec<_> = command[29..].match_indices("/").collect();
     let index_of_subdirectory = indexes_of_slash[1].0;
 
-    let repository_url = &command[10..29 + index_of_subdirectory];
+    let mut username_and_repo = &command[29 + 1..29 + index_of_subdirectory];
+    let index_of_username = username_and_repo.find("/").unwrap();
+    username_and_repo = &username_and_repo[index_of_username + 1..];
+    println!("repo: {}", username_and_repo);
+
+    let repository_subdirectory = &command[29 + index_of_subdirectory + 6..];
+    println!("{}", repository_subdirectory);
+
+    let index_of_branch = repository_subdirectory.find("/").unwrap();
+    let branch: &str = &repository_subdirectory[..index_of_branch];
+    println!("{}", branch);
+
+    let mut checkout_branch_command = "git checkout ".to_owned();
+    checkout_branch_command += branch;
+
+    let subdirectory = &repository_subdirectory[index_of_branch + 1..];
+    println!("{}", subdirectory);
+
+    // let mut change_directory_command = "cd ".to_owned();
+    // change_directory_command += &subdirectory;
+    // println!("{}", change_directory_command);
+
+    let number_of_changes = subdirectory.matches("/").count() + 1;
+    println!("{}", number_of_changes);
+
+    // let mut change_back_directory_command = "cd ".to_owned();
+    // for _ in 0..number_of_changes {
+    //     change_back_directory_command += "../";
+    // }
+    // println!("{}", change_back_directory_command);
+
+    let mut move_command = "mv ".to_owned();
+    move_command += &subdirectory;
+    move_command += &" .";
+    println!("{}", move_command);
+
+    let mut remove_original_directory_command = "rm -rf ".to_owned();
+    remove_original_directory_command += &username_and_repo;
+    println!("{}", remove_original_directory_command);
 
     command = command[..29+index_of_subdirectory].to_string();
 
-    execute_command(&command);
+    execute_command(&command); // clone the repository
+    execute_command(&checkout_branch_command); // checkout the branch
+    execute_command(&move_command); // move the subdirectory to the current directory
+    execute_command(&remove_original_directory_command); // remove the original directory
+
 
     // // Change `ls` to execute in the root directory.
     // list_dir.current_dir("/");
