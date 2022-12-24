6e87a679ef01f4fcdaf9fa6b2ce17aad04d51b91
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-23 20:02:33 -0500

    feat: printing repository

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("{}", "Usage: {cgs <git_subdirectory>}");
+    }
+
+    let query = &args[1];
+
+    let mut command = "git clone ".to_owned();
+    command += query;
+    // !NOTE: command = "git clone https://github.com/vercel/next.js/tree/deprecated-main/examples/active-class-name"
+
+    let indexes_of_slash: Vec<_> = command[29..].match_indices("/").collect();
+    let index_of_subdirectory = indexes_of_slash[1].0;
+
+    let repository_url = &command[10..29 + index_of_subdirectory];
+    println!("{:?}", repository_url);
+
     let mut list_dir = Command::new("ls");
 
     // Execute `ls` in the current directory of the program.
